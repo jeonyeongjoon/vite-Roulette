c27586bb17e40c573a14e8be5fcf38d5ca7dedcb
Yeong joon <bluehome8626@naver.com>
2023-07-26 14:49:27 +0900

    git check

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -43,7 +43,7 @@ function Input() {
         value={item}
         onChange={onChangeInput}
         type="text"
-        placeholder="오늘의 To Do를 입력해주세요!"
+        placeholder="오늘의 To Do를 입력해주세요! git check"
       />
     </form>
   );
